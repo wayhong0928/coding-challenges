b87699facd442f1e3674f1e0d02145f350b7d729
wayhong <cwhong0928@gmail.com>
2025-08-24 21:30:23 +0800

    Update: SQL weather observation station

---
+++ HackerRank/sql/basic_select/weather_observation_station_10.sql
@@ -0,0 +1,5 @@
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+select distinct city from station where city not like '%[aeiou]';
---
+++ HackerRank/sql/basic_select/weather_observation_station_11.sql
@@ -0,0 +1,5 @@
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+
+select distinct city from station where city not like '[aeiou]%' or city not like '%[aeiou]';
